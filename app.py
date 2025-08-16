f23f14c12b8fd771c6b4a9a8141a83ed2bf78184
andrew adel <68513548+andrew-adel-labib@users.noreply.github.com>
2025-08-16 13:34:15 +0300

    Andrew Adel

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-def generate_response(question, llm, temperature, max_tokens):
-    llm = Ollama(model=llm)
+def generate_response(question, model_name, temperature, num_predict):
+    llm = Ollama(model=model_name, temperature=temperature, num_predict=num_predict)
     output_parser = StrOutputParser()
     chain = prompt|llm|output_parser
     answer = chain.invoke({"question": question})
@@ -27,15 +27,15 @@ def generate_response(question, llm, temperature, max_tokens):
 
 st.title("Enhanced Q&A Chatbot with Ollama")
 
-llm = st.sidebar.selectbox("Select an Ollama Model: ", ["gemma2", "mistral", "llama3"])
+llm = st.sidebar.selectbox("Select an Ollama Model: ", ["gemma:2b", "mistral", "llama3:latest"])
 temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
-max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+num_predict = st.sidebar.slider("Number of Predictions", min_value=50, max_value=300, value=150)
 
 st.write("Go ahead and ask any question!")
 user_input = st.text_input("You: ")
 
 if user_input:
-    response = generate_response(user_input, llm, temperature, max_tokens)
+    response = generate_response(user_input, llm, temperature, num_predict)
     st.write(response)
 
 else:
